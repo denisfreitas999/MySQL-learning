6021673b5cd55d84eca08c10b6ec7f065a61b513
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-16 12:23:28 -0300

    feat: :sparkles: Conhecendo projeto Insight Places, instalando MySQL, conhecendo MySQL Workbench, acessando MySQL pela linha de comando.

---
+++ course-01/sql/comandos.sql
@@ -0,0 +1,25 @@
+-- ########################################################
+-- $$$$$$$$$$$$$$$$$$$$$$$ ETAPA 01 $$$$$$$$$$$$$$$$$$$$$$$
+-- ########################################################
+
+-- Acessando MySQL por linha de comando
+/*
+C:\Users\Usuário>cd C:\Program Files\MySQL\MySQL Server 8.0\bin
+Enter password: ********
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 11
+Server version: 8.0.37 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> CREATE DATABASE alura;
+Query OK, 1 row affected (0.05 sec)
+
+mysql>
+*/
