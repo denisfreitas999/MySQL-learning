83c5e64d9ddd6ede487167580f0516968a92566c
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-07-17 16:52:57 -0300

    feat: :sparkles: Modificando métrica de taxa de ocupação com JOIN, GROUP BY e subconsulta, transformando dados para meses e anos com YEAR, MONTH e COUNT, criando análise por região e estado com AVG, MAX e MIN.

--- course-02/sql/comandos.sql
+++ course-02/sql/comandos.sql
@@ -72,4 +72,104 @@ SELECT
     GROUP BY hospedagem_id
     ORDER BY taxa_ocupacao DESC;
 
+-- ########################################################
+-- $$$$$$$$$$$$$$$$$$$$$$$ ETAPA 02 $$$$$$$$$$$$$$$$$$$$$$$
+-- ########################################################
+use insight_places;
+
+-- AGRUPE O TOTAL DE HOSPEDAGEM POR PROPRIETARIO DO MAIOR PARA O MENOR
+SELECT p.nome, COUNT(DISTINCT h.hospedagem_id) AS total_hospedagens
+	FROM proprietarios p
+	JOIN hospedagens h ON(p.proprietario_id = h.proprietario_id)
+    GROUP BY p.proprietario_id
+    ORDER BY total_hospedagens DESC;
+
+use insight_places;
+
+-- taxa de ocupação por proprietário
+SELECT
+    p.nome AS Proprietario,
+    MIN(primeira_data) AS primeira_data,
+    SUM(total_dias) AS total_dias,
+    SUM(dias_ocupados) AS dias_ocupados,
+    ROUND((SUM(dias_ocupados) / SUM(total_dias)) * 100) AS taxa_ocupacao
+FROM(
+    SELECT 
+        hospedagem_id,
+        MIN(data_inicio) AS primeira_data,
+        SUM(DATEDIFF(data_fim, data_inicio)) AS dias_ocupados,
+        DATEDIFF(MAX(data_fim), MIN(data_inicio)) AS total_dias
+    FROM 
+        alugueis
+    GROUP BY 
+        hospedagem_id
+    ) tabela_taxa_ocupacao
+JOIN
+    hospedagens h ON tabela_taxa_ocupacao.hospedagem_id = h.hospedagem_id
+JOIN
+    proprietarios p ON h.proprietario_id = p.proprietario_id
+GROUP BY
+    p.proprietario_id
+ORDER BY
+    total_dias DESC;
+
+-- TOTAL DE ALUGUEIS ANO/MÊS
+
+SELECT
+    YEAR(data_inicio) AS ano,
+    MONTH(data_inicio) AS mes,
+    COUNT(*) AS total_alugueis
+FROM
+    alugueis
+GROUP BY
+    ano, mes
+ORDER BY
+    ano, mes;
     
+-- TOTAL DE ALUGUEIS POR MÊS
+
+SELECT
+    MONTH(data_inicio) AS mes,
+    COUNT(*) AS total_alugueis
+FROM
+    alugueis
+GROUP BY
+    mes
+ORDER BY
+    total_alugueis DESC;
+
+
+-- Média de aluguéis por estado
+
+SELECT
+    e.estado,
+    ROUND(AVG(a.preco_total / DATEDIFF(a.data_fim, a.data_inicio)), 2) AS media_preco_aluguel,
+    ROUND(MAX(a.preco_total / DATEDIFF(a.data_fim, a.data_inicio)), 2) AS max_preco_dia,
+    ROUND(MIN(a.preco_total / DATEDIFF(a.data_fim, a.data_inicio)), 2) AS min_preco_dia,
+    ROUND(AVG(DATEDIFF(a.data_fim, a.data_inicio))) AS media_dias_aluguel
+FROM
+    alugueis a
+JOIN
+    hospedagens h ON a.hospedagem_id = h.hospedagem_id
+JOIN
+    enderecos e ON h.endereco_id = e.endereco_id
+GROUP BY
+    e.estado;
+
+-- Média de aluguel por região
+SELECT
+    r.regiao,
+	ROUND(AVG(a.preco_total / DATEDIFF(a.data_fim, a.data_inicio)), 2) AS media_preco_aluguel,
+    ROUND(MAX(a.preco_total / DATEDIFF(a.data_fim, a.data_inicio)), 2) AS max_preco_dia,
+    ROUND(MIN(a.preco_total / DATEDIFF(a.data_fim, a.data_inicio)), 2) AS min_preco_dia,
+    ROUND(AVG(DATEDIFF(a.data_fim, a.data_inicio))) AS media_dias_aluguel
+FROM
+    alugueis a
+JOIN
+    hospedagens h ON a.hospedagem_id = h.hospedagem_id
+JOIN
+    enderecos e ON h.endereco_id = e.endereco_id
+JOIN 
+    regioes_geograficas r ON r.estado = e.estado
+GROUP BY
+    r.regiao; 
